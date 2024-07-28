b6305c5897d1fec5dd8523142fe9d3baa87e2818
iinhle <Sawcyzee@gmail.com>
2024-07-28 03:11:54 +0200

    Models

--- cakecastle/orders/models.py
+++ cakecastle/orders/models.py
@@ -32,3 +32,22 @@ class Post(models.Model):
 
     def __str__(self):
         return self.title
+
+from django.db import models
+from django.contrib.auth.models import User
+
+class Cake(models.Model):
+    name = models.CharField(max_length=200)
+    flavor = models.CharField(max_length=100)
+    size = models.CharField(max_length=100)
+    price = models.DecimalField(max_digits=5, decimal_places=2)
+    # Other fields...
+
+class Review(models.Model):
+    cake = models.ForeignKey(Cake, on_delete=models.CASCADE, related_name='reviews')
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    rating = models.PositiveSmallIntegerField(choices=[(i, i) for i in range(1, 6)])
+    comment = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+
